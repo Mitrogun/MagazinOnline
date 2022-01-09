9de066b2997a09653d25f5b2647691b5afac0737
admin <admin@DESKTOP-6D3IIJS>
2022-01-09 12:06:50 +0200

    Add project files.

---
+++ Data/MagazinOnlineContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MagazinOnline.Models;
+
+namespace MagazinOnline.Data
+{
+    public class MagazinOnlineContext : DbContext
+    {
+        internal object MarketCap;
+
+        public MagazinOnlineContext (DbContextOptions<MagazinOnlineContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MagazinOnline.Models.Cryptocurrency> Cryptocurrency { get; set; }
+
+        public DbSet<MagazinOnline.Models.Seller> Seller { get; set; }
+
+        public DbSet<MagazinOnline.Models.CryptoMarketCap> CryptoMarketCap { get; set; }
+    }
+}
